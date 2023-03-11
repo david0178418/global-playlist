e7f3b35e602077f36f0505ee39d16fc419745d68
David Granado <davidjgranado@gmail.com>
2023-03-10 22:25:03 -0600

    Add/remove pages from list

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -2,29 +2,43 @@ import React, { useEffect, useState } from 'react';
 import logo from '@assets/img/logo.svg';
 import '@pages/popup/Popup.css';
 
+type Page = chrome.tabs.Tab
+
+interface SavedPage {
+	title: string;
+	url: string;
+}
+
 function Popup() {
-	const [pages, setPages] = useState([]);
+	const [pages, setPages] = useState<SavedPage[]>([]);
+	const [currentPage, setCurrentPage] = useState<Page | null>(null);
+	const currentPageSaved = !!pages.find(p => p.url === currentPage?.url);
 
 	useEffect(() => {
-		console.log('register');
-		const foo: any = ({ pages: { newValue } }) => {
-			console.log(newValue);
-		};
-		chrome.storage.local.onChanged.addListener(foo);
+		(async () => {
+			setPages(await getSavedPages());
+			setCurrentPage(await getCurrentTabs());
+		})();
+	}, []);
 
-		return () => {
-			console.log('unregister');
-			chrome.storage.local.onChanged.removeListener(foo);
-		};
-	}, [pages]);
+	async function handleRemovePage() {
+		if(!currentPage?.url) {
+			return;
+		}
 
-	async function savePage() {
-		const [page] = await chrome.tabs.query({
-			active: true,
-			currentWindow: true,
-		});
+		const newPages = await removePage(currentPage.url);
 
-		await chrome.storage.local.set({ pages: [page] });
+		setPages(newPages);
+	}
+
+	async function handleAddPage() {
+		if(!currentPage) {
+			return;
+		}
+
+		const newPages = await addPage(currentPage);
+
+		setPages(newPages);
 	}
 
 	return (
@@ -39,16 +53,74 @@ function Popup() {
 			</p>
 
 			<p>
-				<button onClick={savePage}>
-					Add to Queue2
-				</button>
-				<button>
-				Remove From Queue
-				</button>
+				{currentPageSaved ? (
+					<button onClick={handleRemovePage}>
+						Remove From Queue
+					</button>
+				) : (
+					<button onClick={handleAddPage}>
+						Add to Queue2
+					</button>
+				)}
 			</p>
-
+			<ul>
+				{pages.map(p => (
+					<li key={p.url}>
+						<a href={p.url}>{p.title}</a>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
 
 export default Popup;
+
+async function getCurrentTabs() {
+	const [ page] = await chrome.tabs.query({
+		active: true,
+		currentWindow: true,
+	});
+
+	return page;
+}
+
+async function getSavedPages(): Promise<SavedPage[]> {
+	const { pages = [] } = await chrome.storage.local.get('pages');
+
+	return pages;
+}
+
+async function addPage(page: Page): Promise<SavedPage[]> {
+	const currentPages = await getSavedPages();
+	const {
+		url,
+		title,
+	} = page;
+
+	if(!(url && title)) {
+		console.log(url, title);
+		return currentPages;
+	}
+
+	const pages = [
+		{
+			url,
+			title,
+		},
+		...currentPages,
+	];
+
+	await chrome.storage.local.set({ pages });
+
+	return pages;
+}
+
+async function removePage(url: string) {
+	const currentPages = await getSavedPages();
+	const pages = currentPages.filter(p => p.url !== url);
+
+	await chrome.storage.local.set({ pages });
+
+	return pages;
+}
